608f7296c51a0fb8437b820837e47270a20aa66b
linhnvph36809 <linhnvph36809@fpt.edu.vn>
2024-09-19 14:03:33 +0700

    fix login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,59 @@
 "use client";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import { Button, Form, Input } from "antd";
+import { useRouter } from "next/navigation";
+
 import "antd/dist/reset.css";
 import useAuth from "@/app/hooks/useAuth";
-import { useTranslation } from "react-i18next";
+import { handlerGetCookie } from "./cookies";
 
 const Login = () => {
   const { t } = useTranslation();
   const { handlerLogin } = useAuth();
 
-  return (
-    <>
-      <section className="w-[200px] h-[100vh] mx-auto flex justify-center items-center">
-        <div>
-          <h1 className="text-[50px] font-medium text-center mb-6">
-            {t("login_message.title")}
-          </h1>
-          <Form
-            layout="vertical"
-            name="basic"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={handlerLogin}
-          >
-            <Form.Item
-              label={<div>{t("login_message.label_username")}</div>}
-              name="username"
-            >
-              <Input className="w-[250px] h-[40px] bg-white" />
-            </Form.Item>
+  const router = useRouter();
 
-            <Form.Item className="text-center">
-              <Button
-                type="primary"
-                className="w-[150px] h-[35px]"
-                htmlType="submit"
-              >
-                {t("login_message.btn_message")}
-              </Button>
-            </Form.Item>
-          </Form>
-        </div>
-      </section>
-    </>
+  useEffect(() => {
+    const token = handlerGetCookie("accessToken");
+    if (token) {
+      router.push("/pages/home");
+    }
+  }, [router]);
+
+  return (
+    <section className="w-[200px] h-[100vh] mx-auto flex justify-center items-center">
+      <div>
+        <h1 className="text-[50px] font-medium text-center mb-6">
+          {t("login_message.title")}
+        </h1>
+        <Form
+          layout="vertical"
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={handlerLogin}
+        >
+          <Form.Item
+            label={<div>{t("login_message.label_username")}</div>}
+            name="username"
+          >
+            <Input className="w-[250px] h-[40px] bg-white" />
+          </Form.Item>
+
+          <Form.Item className="text-center">
+            <Button
+              type="primary"
+              className="w-[150px] h-[35px]"
+              htmlType="submit"
+            >
+              {t("login_message.btn_message")}
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </section>
   );
 };
 
